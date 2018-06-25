ca8b55eebd5a31f0a2239d74e2cbfab25680d984
Sergio Ruiz <sergio.ruiz@bizneo.com>
2018-06-25 18:37:34 +0200

    Finished

--- src/com/volumen/Main.java
+++ src/com/volumen/Main.java
@@ -3,8 +3,15 @@ package com.volumen;
 public class Main {
 
     public static void main(String[] args) {
-        Volumen volumen = new Volumen(0.0, 1.0);
+        int iteraciones = 100000;
+        long startTime, endTime;
 
-        System.out.println(volumen.areaP(1000000));
+        Volumen volumen = new Volumen(0.0, 1.0, 0.0, 1.0);
+
+        startTime = System.currentTimeMillis();
+        System.out.println(volumen.areaValoresMedios(iteraciones));
+        endTime = System.currentTimeMillis();
+
+        System.out.println("Tiempo para " + iteraciones + " iteraciones: " + (endTime-startTime) + "ms");
     }
 }
--- src/com/volumen/Volumen.java
+++ src/com/volumen/Volumen.java
@@ -1,26 +1,47 @@
 package com.volumen;
 
 public class Volumen {
-    double limI, limS, amplitud;
+    double limIx, limSx, limIy, limSy, amplitudx, amplitudy;
 
-    public Volumen(double limInferior, double limSuperior) {
-        limI = limInferior;
-        limS = limSuperior;
-        amplitud = limS - limI;
+    public Volumen(double limInferiorX, double limSuperiorX, double limInferiorY, double limSuperiorY) {
+        limIx = limInferiorX;
+        limSx = limSuperiorX;
+        limIy = limInferiorY;
+        limSy = limSuperiorY;
+        amplitudx = limSx - limIx;
+        amplitudy = limSy - limIy;
     }
-    public double areaP(int k) {
+
+    public double areaValoresMedios(int k) {
+        double[] valores = new double[k]; //para calcular medias, cuasi varianzas e intervalos de confianza
+        double suma = 0.0;
+
+        for (int i = 0; i < k; i++) {
+            double x = Math.random() * amplitudx + limIx;
+            double y = Math.random() * amplitudy+ limIy;
+            double z = fun(x, y);
+
+            valores[i] = amplitudx * amplitudy * z;
+            suma = suma + valores[i];
+        }
+
+        intervaloConfianza(valores);
+        return suma/k;
+    }
+
+    public double areaProporcion(int k) {
         int buenos = 0;
 
         for (int i = 0; i < k; i++) {
-            double x = Math.random() * amplitud + limI;
-            double y = Math.random() * amplitud + limI;
-            double z = Math.random() * fun(limS, limS);
+            double x = Math.random() * amplitudx+ limIx;
+            double y = Math.random() * amplitudy + limIy;
+            double z = Math.random() * fun(limSx, limSy);
 
             if (z <= fun(x, y)) buenos++;
         }
-        IntervaloConfProporciones(buenos/(double)k, k);
+        IntervaloConfProporciones((double)buenos/k, k);
 
-        return amplitud*fun(limS, limS)* buenos/(double)k;
+        return amplitudx * amplitudy * fun(limSx, limSy) * buenos/(double)k;
     }
 
     private double fun(double x, double y) {
@@ -31,6 +52,30 @@ public class Volumen {
         double intervaloInf = p - 1.96* Math.sqrt (p*(1 - p)/n);
         double intervaloSup = p+1.96* Math.sqrt (p*(1 - p)/n);
 
+        System.out.println("Intervalo para proporción: ["+ intervaloInf +" , "+ intervaloSup +"]");
+    }
+
+    public void intervaloConfianza ( double [] valores){// valores.length tamaño de la muestra
+        double media=media(valores);
+        double S= cuasiV (valores, media);
+        double intervaloInf = media - 1.96*S/ Math.sqrt ( valores.length );
+        double intervaloSup = media+1.96*S/ Math.sqrt ( valores.length );
         System.out.println("Intervalo: ["+ intervaloInf +" , "+ intervaloSup +"]");
+    }
+
+    public double cuasiV ( double [] valores, double media){
+        double S=0;
+        for ( int x=0;x< valores.length;x ++){
+            S= S+Math.pow (valores[x] - media,2);
+        }
+        return Math.sqrt (S/(valores.length - 1));
+    }
+
+    public double media( double valores[]){
+        double media=0;
+        for ( int x=0;x< valores.length;x ++){
+            media= media+valores [x];
+        }
+        return media/ valores.length ;
     }
 }
